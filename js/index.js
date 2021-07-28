7e3f2c53ad68841818c91a06506c47014ac516a1
nicolemerinotsui <nmerinotsui@gmail.com>
2021-07-28 11:46:21 -0700

    Add index.js

---
+++ js/index.js
@@ -0,0 +1,68 @@
+let today=new Date()
+
+today.getFullYear()
+
+let thisYear=(today.getFullYear())
+
+let footer=document.querySelector("footer")
+footer.style ="border: dotted;"
+
+let copyright=document.createElement("p")
+copyright.innerHTML= `Nicole Merino Tsui ${thisYear}`  
+footer.appendChild(copyright)
+
+const skills =['Program Management', 'Full Stack', 'International Development', 'Transitional Team Building', 'Monitoring and Evaluation'];
+let skillsSection=document.getElementById("skills")
+let skillsList=skillsSection.querySelector("ul")
+
+for (let i = 0; i < skills.length; i++) {
+let skill= document.createElement("li");
+skill.innerText=skills[i];
+skillsList.appendChild(skill)
+}
+
+
+
+// let messageForm=document.getElementById("form")
+//let messageForm =document.querySelector("leave_message")
+// messageForm.addEventListener('submit', (e) => {
+//     console.log('submit');
+// })
+const messageForm = document.querySelector("[name='leave_message']");
+//console.log(messageForm);
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+		const form = e.target;
+		const name = document.getElementById('nameform').value;
+		const email = document.getElementById('emailname').value;
+		const message = document.getElementById('formMessage').value;
+        console.log(name, email, message);
+
+        // Display Messages in List
+        let messageSection = document.getElementById('messages');
+        let messageList = messageSection.querySelector('ul');
+        let newMessage = document.createElement('li'); 
+        newMessage.innerHTML = 
+            `<a href=“mailto:${email}”>${name}</a>
+            <span id ="spanMessage"> wrote: ${message}</span>`;
+        
+        // Add remove button to each message
+        let removeButton = document.createElement('button');
+        removeButton.innerHTML = 'remove';
+        removeButton.type = 'button'; 
+        
+        removeButton.addEventListener('click', (e) => {
+            let entry = e.target.parentNode;
+            entry.remove();
+        });
+
+        // Adding the remove button and message to messageList
+        newMessage.appendChild(removeButton);
+        messageList.appendChild(newMessage);
+
+        messageForm.reset();
+    });
+
+
+
+
